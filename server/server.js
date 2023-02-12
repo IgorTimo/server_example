8634120f8cafde7df9c437691d4656068d925cc6
IgorTimo <rostovplace@yandex.ru>
2023-02-12 10:18:23 +0300

    turn off cors

--- server/server.js
+++ server/server.js
@@ -17,11 +17,11 @@ const DiaryRecord = new mongoose.model("DiaryRecord", diaryRecord);
 
 var corsOptions = {
   origin: 'http://localhost:3000',
-  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+  optionsSuccessStatus: 200 
 }
 
 const app = express();
-app.use(cors(corsOptions));
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded());
 
